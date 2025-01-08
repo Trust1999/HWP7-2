e747382321c53a33ce1d1511a6d14ec073f64afe
Trust1999 <159055268+Trust1999@users.noreply.github.com>
2025-01-08 16:46:32 +0100

    Update ICommunicationInterface.hpp

--- ICommunicationInterface.hpp
+++ ICommunicationInterface.hpp
@@ -1,19 +1,16 @@
 #pragma once
 
 #include <cstdint>
-#include <mutex>
 
 class ICommunicationInterface
 {
-protected:
-	mutable std::mutex mtx;
-
-public:
+	
+	public:
 	virtual ~ICommunicationInterface() = default;
 
 	virtual void writeByte(uint8_t data) = 0;
 
-	virtual std::optional<uint8_t> readByte() = 0;
+	virtual uint8_t readByte() = 0;
 
 	virtual void openCom() = 0;
 
